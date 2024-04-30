e547a1a38a6252b49f51e87bf0701136aa6dddfa
Prithviraj Burman <prithviraj.burman107@gmail.com>
2024-04-30 17:30:15 +0100

    set up  part a need to set up endpoints

---
+++ part-b-frontend-app/src/main/java/com/example/Comp2005_b/Comp2005BApplication.java
@@ -0,0 +1,13 @@
+package com.example.Comp2005_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp2005BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp2005BApplication.class, args);
+	}
+
+}
---
+++ part-b-frontend-app/src/test/java/com/example/Comp2005_b/Comp2005BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Comp2005_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp2005BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
